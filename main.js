a824883aab3dc334ece2b164457582f77d446448
Hans867 <hans867m@stud.kea.dk>
2022-11-21 10:38:16 +0100

    JOHNNY!!!

--- main.js
+++ main.js
@@ -13,9 +13,16 @@ function renderBars(bars) {
         const li = document.createElement('li');
         li.innerHTML = `
             <p class="text">${bar.bar_name}</p>
-            <p class="price">${bar.bar_location}</p>
-            <p class="price">${bar.bar_city}</p>
-            <p class="price">${bar.bar_description}</p>
+            <p class="bar_location">${bar.bar_location}</p>
+            <p class="bar_city">${bar.bar_city}</p>
+            <p class="bar_description">${bar.bar_description}</p>
+            <p class="opening_hours">${bar.opening_hours}</p>
+            <p class="prices">${bar.prices}</p>
+            <p class="games">${bar.games}</p>
+            <p class="billiard">${bar.billiard}</p>
+            <p class="smoking_allowed">${bar.smoking_allowed}</p>
+            <p class="happy_hour">${bar.happy_hours}</p>
+            <p class="viser_sport">${bar.viser_sport}</p>
         `;
         ul.appendChild(li);
     }
